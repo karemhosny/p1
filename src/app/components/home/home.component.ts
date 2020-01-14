379bd5e7e91081875d795b61f93cb3f11651aaaa
unknown <karemhosny111@gmail.com>
2020-01-14 12:29:08 +0200

    node wrapper

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -33,6 +33,23 @@ branches:any=[
   { address:{title:'Piccadlly Alsalymia',details:'Piccadlly Alsalymia details'},phone:'22657201', mapSrc:"https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d434.7987217240207!2d48.02877995501674!3d29.32957073533545!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3fcf9c59907baaab%3A0xa6dcddfc11481d6b!2srelax%20shoes%20co.!5e0!3m2!1sen!2seg!4v1578136782353!5m2!1sen!2seg"}
  
 ]
+clients:any=[
+  {name:"client1",imgSrc:"../assets/img/clients/client-2.png"},
+ {name:"client2",imgSrc:"../assets/img/clients/client-3.png"},
+ {name:"client3",imgSrc:"../assets/img/clients/client-4.png"},
+ {name:"client4",imgSrc:"../assets/img/clients/client-5.png"},
+ {name:"client5",imgSrc:"../assets/img/clients/client-6.png"},
+ {name:"client6",imgSrc:"../assets/img/clients/client-7.png"},
+ {name:"client7",imgSrc:"../assets/img/clients/client-8.png"},
+ {name:"client8",imgSrc:"../assets/img/clients/client-1.png"},
+ {name:"client9",imgSrc:"../assets/img/clients/client-2.png"},
+ {name:"client10",imgSrc:"../assets/img/clients/client-3.png"},
+ {name:"client11",imgSrc:"../assets/img/clients/client-4.png"},
+ {name:"client12",imgSrc:"../assets/img/clients/client-5.png"},
+ {name:"client13",imgSrc:"../assets/img/clients/client-6.png"},
+ {name:"client14",imgSrc:"../assets/img/clients/client-7.png"},
+ {name:"client15",imgSrc:"../assets/img/clients/client-8.png"}
+]
   public carouselTileItems$: Observable<number[]>;
   public carouselTileConfig: NguCarouselConfig = {
     grid: { xs: 1, sm: 1, md: 3, lg: 4, all: 0 },
@@ -68,8 +85,7 @@ branches:any=[
 allProducts:any=[]
 products:any=[]
 allClients:any=[];
-clients:any=[];
-  //create form for send mails
+//clients:any=[];
   profileForm = this.fb.group({
     name: [''],
     email: [''],
@@ -86,11 +102,11 @@ clients:any=[];
       this.products = data['products'];
       this.allProducts=this.products;
     });
-    this.apiService.getAllClients().subscribe((data)=>{
-      console.log(data);
-      this.clients = data['clients'];
-      this.allClients=this.clients;
-    });
+    // this.apiService.getAllClients().subscribe((data)=>{
+    //   console.log(data);
+    //   this.clients = data['clients'];
+    //   this.allClients=this.clients;
+    // });
   }
   send() {
     console.log(this.profileForm.value);

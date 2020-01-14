42b90fc5766c36db2ddca42333165995d51f5343
unknown <karemhosny111@gmail.com>
2020-01-14 12:11:59 +0200

    node wrapper

--- server.js
+++ server.js
@@ -4,9 +4,9 @@ const path = require("path")
 const app = express();
 // Run the app by serving the static files
 // in the dist directory
-app.use(express.static(__dirname + '/dist/appDeploy'));
+app.use(express.static(__dirname + '/dist/Piccadilly'));
 app.get("/*",(req,res)=>{
-res.sendFile(path.join(__dirname+'/dist/appDeploy/index.html'))
+res.sendFile(path.join(__dirname+'/dist/Piccadilly/index.html'))
 })
 // Start the app by listening on the default
 // Heroku port
